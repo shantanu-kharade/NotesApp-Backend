66e7f271d2b76301b984492dcbc48cd28b36fe41
shantanu-kharade <shantanu.kharade27@gmail.com>
2025-11-18 16:21:51 +0530

    fixed issue with CORS policy

--- index.js
+++ index.js
@@ -10,23 +10,7 @@ const port = process.env.port
 connectDB()
 
 app.use(cors());
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://notes-app-ruddy-five.vercel.app/'
-]
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
 app.use(express.json())
-
 app.use('/Notes', Notes)
 
 app.get('/', (req, res) => {
